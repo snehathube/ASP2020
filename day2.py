58d50345aefebd766200f527d338b542420fde22
Thube <sneha.thube@ifw-kiel.de>
2020-08-18 11:24:33 +0200

    code from day1

--- day2.py
+++ day2.py
@@ -7,4 +7,9 @@ d = {'asp':[2020, 2021,2022], 'ifw': [1990, 'x', 2010]}
 df = pd.DataFrame(d)
 # right click on df --> 'View as dataframe'
 # Stop console should be followed by Rerun (which really means restart the kernel)
-# new change for commit
+
+# overleaf also uses GIT. ny text formats are asy for GIT to check files and compare from.
+
+# Create github account
+# add a gitignore to ignore a typical set of file extensions that you would like to ignore. All temp, auxilliary files
+# it is good for cleanup.
