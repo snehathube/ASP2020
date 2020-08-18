9a09e64b6231783ce68303880115f73bea21326f
Thube <sneha.thube@ifw-kiel.de>
2020-08-18 10:47:23 +0200

    code from day2

---
+++ day2.py
@@ -0,0 +1,10 @@
+"""code from day2 of ASP 2020 course"""
+# Imports
+import pandas as pd
+# Main code  create a toy dataframe
+d = {'asp':[2020, 2021,2022], 'ifw': [1990, 'x', 2010]}
+
+df = pd.DataFrame(d)
+# right click on df --> 'View as dataframe'
+# Stop console should be followed by Rerun (which really means restart the kernel)
+# new change for commit
